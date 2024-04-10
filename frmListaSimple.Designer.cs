a7b03c5fff0c74cb5315be6800eca9ac536fddea
Martina Ponce de Leon <MARTIPONCEDELEON03@ies21.onmicrosoft.com>
2024-04-10 10:25:22 -0300

    Terminar

---
+++ frmListaSimple.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace pryPonceDeLeonMartinaEstrucDatos
+{
+    partial class frmListaSimple
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmListaSimple));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.grillaListaSimple = new System.Windows.Forms.DataGridView();
+            this.clmCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmTramite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lstLista = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtTramite = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picCola = new System.Windows.Forms.PictureBox();
+            this.cboLista = new System.Windows.Forms.ComboBox();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grillaListaSimple)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCola)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.grillaListaSimple);
+            this.groupBox3.Controls.Add(this.lstLista);
+            this.groupBox3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(33, 411);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(868, 268);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Listado en una Lista y una Grilla";
+            // 
+            // grillaListaSimple
+            // 
+            this.grillaListaSimple.AllowUserToAddRows = false;
+            this.grillaListaSimple.AllowUserToDeleteRows = false;
+            this.grillaListaSimple.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.grillaListaSimple.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grillaListaSimple.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmCodigo,
+            this.clmNombre,
+            this.clmTramite});
+            this.grillaListaSimple.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.grillaListaSimple.Location = new System.Drawing.Point(392, 31);
+            this.grillaListaSimple.Name = "grillaListaSimple";
+            this.grillaListaSimple.ReadOnly = true;
+            this.grillaListaSimple.Size = new System.Drawing.Size(460, 220);
+            this.grillaListaSimple.TabIndex = 1;
+            // 
+            // clmCodigo
+            // 
+            this.clmCodigo.HeaderText = "Código";
+            this.clmCodigo.Name = "clmCodigo";
+            this.clmCodigo.ReadOnly = true;
+            // 
+            // clmNombre
+            // 
+            this.clmNombre.HeaderText = "Nombre";
+            this.clmNombre.Name = "clmNombre";
+            this.clmNombre.ReadOnly = true;
+            this.clmNombre.Width = 200;
+            // 
+            // clmTramite
+            // 
+            this.clmTramite.HeaderText = "Trámite";
+            this.clmTramite.Name = "clmTramite";
+            this.clmTramite.ReadOnly = true;
+            // 
+            // lstLista
+            // 
+            this.lstLista.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.ItemHeight = 15;
+            this.lstLista.Location = new System.Drawing.Point(17, 31);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(333, 214);
+            this.lstLista.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cboLista);
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(681, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(220, 350);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Elemento Eliminado";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnEliminar.Location = new System.Drawing.Point(51, 270);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(114, 39);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTramite);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(425, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 350);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nuevo Elemento";
+            // 
+            // txtTramite
+            // 
+            this.txtTramite.Location = new System.Drawing.Point(107, 179);
+            this.txtTramite.Name = "txtTramite";
+            this.txtTramite.Size = new System.Drawing.Size(100, 23);
+            this.txtTramite.TabIndex = 15;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(105, 111);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 23);
+            this.txtNombre.TabIndex = 14;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(105, 53);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 23);
+            this.txtCodigo.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Trámite:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnAgregar.Location = new System.Drawing.Point(52, 270);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(114, 39);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Código:";
+            // 
+            // picCola
+            // 
+            this.picCola.Image = ((System.Drawing.Image)(resources.GetObject("picCola.Image")));
+            this.picCola.Location = new System.Drawing.Point(33, 22);
+            this.picCola.Name = "picCola";
+            this.picCola.Size = new System.Drawing.Size(350, 350);
+            this.picCola.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCola.TabIndex = 8;
+            this.picCola.TabStop = false;
+            // 
+            // cboLista
+            // 
+            this.cboLista.FormattingEnabled = true;
+            this.cboLista.Location = new System.Drawing.Point(44, 179);
+            this.cboLista.Name = "cboLista";
+            this.cboLista.Size = new System.Drawing.Size(130, 23);
+            this.cboLista.TabIndex = 10;
+            this.cboLista.SelectedIndexChanged += new System.EventHandler(this.cboLista_SelectedIndexChanged);
+            // 
+            // frmListaSimple
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(934, 701);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picCola);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "frmListaSimple";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Estructura Dinámica Lineal: Lista Simple";
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grillaListaSimple)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCola)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView grillaListaSimple;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTramite;
+        private System.Windows.Forms.ListBox lstLista;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cboLista;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTramite;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picCola;
+    }
+}
---
+++ frmListaSimple.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPonceDeLeonMartinaEstrucDatos
+{
+    public partial class frmListaSimple : Form
+    {
+        public frmListaSimple()
+        {
+            InitializeComponent();
+        }
+
+        clsListaSimple Lista = new clsListaSimple();
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (Lista.Primero != null)
+            {
+                Int32 x = Convert.ToInt32(cboLista.Text);
+                Lista.Eliminar(x);
+                Lista.Recorrer(grillaListaSimple);
+                Lista.Recorrer(lstLista);
+                Lista.Recorrer(cboLista);
+                Lista.Recorrer();
+            }
+            else
+            {
+                MessageBox.Show("La lista está vacía");
+            }
+            btnEliminar.Enabled = false;    
+        }
+
+        private void cboLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboLista.Text == "")
+            {
+                btnEliminar.Enabled = false;
+
+            }
+            else
+            {
+                btnEliminar.Enabled  = true;
+            }
+        }
+    }
+}
