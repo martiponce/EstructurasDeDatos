aec50d0c226662a855ff874c7a994d33f81f8f11
Martina Ponce de Leon <MARTIPONCEDELEON03@ies21.onmicrosoft.com>
2024-05-20 20:09:45 -0300

    Terninado el repaso, no andan los años

--- frmBaseDatosRepasoOperaciones.Designer.cs
+++ frmBaseDatosRepasoOperaciones.Designer.cs
@@ -32,8 +32,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.cboOperaciones = new System.Windows.Forms.ComboBox();
             this.btnListar = new System.Windows.Forms.Button();
-            this.txtQuery = new System.Windows.Forms.TextBox();
             this.dgvConsulta = new System.Windows.Forms.DataGridView();
+            this.lblEnunciado = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,6 +49,7 @@
             // 
             // cboOperaciones
             // 
+            this.cboOperaciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboOperaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboOperaciones.FormattingEnabled = true;
             this.cboOperaciones.Location = new System.Drawing.Point(343, 23);
@@ -66,23 +67,27 @@
             this.btnListar.TabIndex = 2;
             this.btnListar.Text = "LISTAR";
             this.btnListar.UseVisualStyleBackColor = false;
-            // 
-            // txtQuery
-            // 
-            this.txtQuery.Location = new System.Drawing.Point(24, 73);
-            this.txtQuery.Multiline = true;
-            this.txtQuery.Name = "txtQuery";
-            this.txtQuery.Size = new System.Drawing.Size(894, 242);
-            this.txtQuery.TabIndex = 3;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
             // 
             // dgvConsulta
             // 
             this.dgvConsulta.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.dgvConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvConsulta.Location = new System.Drawing.Point(24, 337);
+            this.dgvConsulta.Location = new System.Drawing.Point(24, 275);
             this.dgvConsulta.Name = "dgvConsulta";
-            this.dgvConsulta.Size = new System.Drawing.Size(894, 348);
+            this.dgvConsulta.Size = new System.Drawing.Size(894, 410);
             this.dgvConsulta.TabIndex = 4;
+            // 
+            // lblEnunciado
+            // 
+            this.lblEnunciado.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblEnunciado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEnunciado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnunciado.Location = new System.Drawing.Point(24, 65);
+            this.lblEnunciado.Name = "lblEnunciado";
+            this.lblEnunciado.Size = new System.Drawing.Size(894, 193);
+            this.lblEnunciado.TabIndex = 0;
+            this.lblEnunciado.UseMnemonic = false;
             // 
             // frmBaseDatosRepasoOperaciones
             // 
@@ -91,9 +96,9 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(934, 711);
             this.Controls.Add(this.dgvConsulta);
-            this.Controls.Add(this.txtQuery);
             this.Controls.Add(this.btnListar);
             this.Controls.Add(this.cboOperaciones);
+            this.Controls.Add(this.lblEnunciado);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -101,6 +106,7 @@
             this.Name = "frmBaseDatosRepasoOperaciones";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Base Datos Repaso Operaciones";
+            this.Load += new System.EventHandler(this.frmBaseDatosRepasoOperaciones_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -112,7 +118,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cboOperaciones;
         private System.Windows.Forms.Button btnListar;
-        private System.Windows.Forms.TextBox txtQuery;
         private System.Windows.Forms.DataGridView dgvConsulta;
+        private System.Windows.Forms.Label lblEnunciado;
     }
 }
--- frmBaseDatosRepasoOperaciones.cs
+++ frmBaseDatosRepasoOperaciones.cs
@@ -15,6 +15,131 @@ namespace pryPonceDeLeonMartinaEstrucDatos
         public frmBaseDatosRepasoOperaciones()
         {
             InitializeComponent();
+            LlenarComboBox();
         }
-    }
+
+        private void frmBaseDatosRepasoOperaciones_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void LlenarComboBox()
+        {
+            // 20 situaciones en el comb
+            cboOperaciones.Items.Add("Paises sin libros");
+            cboOperaciones.Items.Add("Paises con libros");
+            cboOperaciones.Items.Add("Libros por autor (ID=2)");
+            cboOperaciones.Items.Add("Proyección múltiple (Título, Autor, Año)");
+            cboOperaciones.Items.Add("Libros en idioma (ID=1)");
+            cboOperaciones.Items.Add("Libros después del 2000");
+            cboOperaciones.Items.Add("Libros antes del 1990");
+            cboOperaciones.Items.Add("Libros con precio mayor a 20");
+            cboOperaciones.Items.Add("Libros con precio menor a 10");
+            cboOperaciones.Items.Add("Autores que empiezan con 'A'");
+            cboOperaciones.Items.Add("Autores que terminan con 'Z'");
+            cboOperaciones.Items.Add("Títulos que contienen 'Ciencia'");
+            cboOperaciones.Items.Add("Títulos que contienen 'Historia'");
+            cboOperaciones.Items.Add("Libros por autores específicos (1, 3, 5)");
+            cboOperaciones.Items.Add("Libros en idiomas específicos (2, 4, 6)");
+            cboOperaciones.Items.Add("Libros entre 1990 y 2000");
+            cboOperaciones.Items.Add("Libros con precio entre 10 y 20");
+            cboOperaciones.Items.Add("Autores que no empiezan con 'J'");
+            cboOperaciones.Items.Add("Títulos que no contienen 'Matemáticas'");
+            cboOperaciones.Items.Add("Libros con precio < 15 y después del 2010");
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            clsBaseDeDatos objBD = new clsBaseDeDatos();
+            String sql = "SELECT * FROM LIBRO";
+            switch (cboOperaciones.SelectedIndex)
+            {
+                case 0:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM PAIS WHERE IDPAIS NOT IN (SELECT DISTINCT IDPAIS FROM LIBRO)";
+                    break;
+                case 1:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM PAIS WHERE IDPAIS IN (SELECT DISTINCT IDPAIS FROM LIBRO)";
+                    break;
+                case 2:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDAUTOR = 2";
+                    break;
+                case 3:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT TITULO, IDAUTOR, AÑO FROM LIBRO ORDER BY TITULO DESC";
+                    break;
+                case 4:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDIDIOMA = 1";
+                    break;
+                case 5:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE AÑO > 2000";
+                    break;
+                case 6:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE AÑO < 1990";
+                    break;
+                case 7:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE PRECIO > 20";
+                    break;
+                case 8:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE PRECIO < 10";
+                    break;
+                case 9:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDAUTOR LIKE 'A%'";
+                    break;
+                case 10:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDAUTOR LIKE '%Z'";
+                    break;
+                case 11:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE TITULO LIKE '%Ciencia%'";
+                    break;
+                case 12:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE TITULO LIKE '%Historia%'";
+                    break;
+                case 13:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDAUTOR IN (1, 3, 5)";
+                    break;
+                case 14:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDIDIOMA IN (2, 4, 6)";
+                    break;
+                case 15:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE AÑO BETWEEN 1990 AND 2000";
+                    break;
+                case 16:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE PRECIO BETWEEN 10 AND 20";
+                    break;
+                case 17:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE IDAUTOR NOT LIKE 'J%'";
+                    break;
+                case 18:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE TITULO NOT LIKE '%Matemáticas%'";
+                    break;
+                case 19:
+                    lblEnunciado.Text = cboOperaciones.Text + ": ";
+                    sql = "SELECT * FROM LIBRO WHERE PRECIO < 15 AND AÑO > 2010";
+                    break;
+                default:
+                    lblEnunciado.Text = "Seleccione una operación válida.";
+                    sql = "SELECT * FROM LIBRO";
+                    break;
+            }
+
+            objBD.Listar(dgvConsulta, sql);
+        }
+    }       
 }
